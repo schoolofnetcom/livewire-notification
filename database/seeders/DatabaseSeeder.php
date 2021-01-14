a099baf1c4433dec0ee5d85e8bc57e34ef37fcf6
thivalls <olavalls@gmail.com>
2021-01-14 19:40:33 -0300

    Preparing environment to dp

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,13 +6,12 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(1)->create([
+            'name' => 'Thiago',
+            'email' => 'thiago@son.com',
+        ]);
+        \App\Models\Book::factory(200)->create();
     }
 }
